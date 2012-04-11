e732350f3cdae6511632a4c4aeb7a3ac2a0e5b9a
choupart <lauriane.colas@gmail.com>
2012-04-11 13:48:13 +0200

    .

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,20 @@
 class Product < ActiveRecord::Base
+
+	validates_presence_of :title, :description, :image_url, :price
+	validates_numericality_of :price
+	validates_uniqueness_of :title
+
+	validates_format_of :image_url, 
+						:with => %r{\.(gif|png|jpg)$}i,
+						:message => "L'image doit etre en gif, png et jpg"
+
+	validate :price_must_be_at_least_a_cent
+
+
+	def price_must_be_at_least_a_cent
+		if self.price.nil? || self.price < 0.01
+			self.errors.add(:price, "He, tu veux te ruiner ?")
+		end
+	end
+
 end
