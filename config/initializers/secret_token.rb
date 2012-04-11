2ba23955f62b485e0ddcee720ee60ae170aa8a64
choupart <lauriane.colas@gmail.com>
2012-04-11 10:22:48 +0200

    site e-commerce en Ruby

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Commerce::Application.config.secret_token = '7efe4e1ad35ee2efd36310d72ea3b463016554b8636f5b8237000762af01ca97dc5808305c35144f5fab886e4a1664883538a20cfd8aae75616b08c917707689'
